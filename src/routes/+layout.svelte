b2bd64b204fcb3e00cd14a6dcc8b9bd4772250bc
itspangler <itspangler@gmail.com>
2025-06-24 15:29:03 -0400

    button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 <div class="text-center bg-gray-100 top-0">
   <h1 class="text-4xl p-6 font-black">Boston icons</h1>
   <p class="italic p-6 rounded">
-    check out the <strong>{icons.length}</strong> icons from Meghan's workshop! (click an icon to make them grayscale)
+    check out the <strong>{icons.length}</strong> icons from Meghan's workshop!
   </p>
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,16 +7,22 @@
   }
 </script>
 
+<div class="fixed top-4 right-4">
+    <button onclick={toggle} class="hover:cursor-pointer z-index-5000 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full box-shadow-lg {!grayscale ? 'filter grayscale' : ''}">
+    🌈
+  </button>
+</div>
+
 <div class="grid grid-flow-col m-12 gap-2 grid-rows-32 md:grid-rows-10">
   {#each icons as icon}
     <div class="object-contain border-gray-500">
-      <button onclick={toggle}>
+      <a target="blank" href="/images/{icon.path}">
         <img
           class="hover:brightness-50 transition rounded hover:cursor-pointer {grayscale ? 'filter grayscale' : ''}"
           src="images/{icon.path}"
           alt="icon"
         />
-      </button>
+      </a>
     </div>
   {/each}
 </div>
