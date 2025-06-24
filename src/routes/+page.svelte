36715bbae5d56bd6fd4f0b507e70873da02eb00c
itspangler <itspangler@gmail.com>
2025-06-24 15:02:14 -0400

    responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,10 @@
   }
 </script>
 
-<div class="grid grid-flow-col grid-rows-10 m-12 gap-2">
+<div class="grid grid-flow-col m-12 gap-2 grid-rows-32 md:grid-rows-10">
   {#each icons as icon}
     <div class="object-contain border-gray-500">
-      <button on:click={toggle}>
+      <button onclick={toggle}>
         <img
           class="hover:brightness-50 transition rounded hover:cursor-pointer {grayscale ? 'filter grayscale' : ''}"
           src="images/{icon.path}"
