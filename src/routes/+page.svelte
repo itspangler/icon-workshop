1dc8e9be7cd13dbda4442dbc69c47d4cc22b09be
itspangler <itspangler@gmail.com>
2025-06-24 11:53:54 -0400

    initialize

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { icons } from "$lib/icons";
+  // import { onMount } from "svelte";
+</script>
+
+<div class="text-center bg-gray-100 top-0">
+  <h1 class="text-4xl p-6 font-black">Boston icons</h1>
+  <p class="italic p-6 rounded">
+    check out the <strong>{icons.length}</strong> icons from Meghan's workshop!
+  </p>
+</div>
+
+<div class="grid grid-flow-col grid-rows-8 m-12 gap-2">
+  {#each icons as icon}
+    <container
+      class="object-contain border-gray-500 hover:brightness-50 transition hover:cursor-pointer"
+    >
+      <img class="rounded" src="../images/{icon.path}" />
+    </container>
+  {/each}
+</div>
