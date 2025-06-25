7ffd57fed10778a403e38650208121edafc8240d
itspangler <itspangler@gmail.com>
2025-06-24 21:46:46 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,7 @@
 <div class="grid grid-flow-col m-12 gap-2 grid-rows-32 md:grid-rows-10">
   {#each icons as icon}
     <div class="object-contain border-gray-500">
-      <a target="blank" href="icon-workshop/images/{icon.path}">
+      <a target="blank" href="images/{icon.path}">
         <img
           class="hover:brightness-50 transition rounded hover:cursor-pointer {grayscale ? 'filter grayscale' : ''}"
           src="images/{icon.path}"
